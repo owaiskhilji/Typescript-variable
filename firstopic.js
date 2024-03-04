1b145d8d4deea21d6d654e7492abb13c7ba5df40
owaiskhilji <owaiskhilji6@gmail.com>
2024-03-05 01:41:56 +0500

    variable lesson

---
+++ firstopic.js
@@ -0,0 +1,31 @@
+"use strict";
+// let first_name: string = 'owais'
+// console.log(first_name);
+Object.defineProperty(exports, "__esModule", { value: true });
+//Datatype types
+// string;
+// number;
+// Boolean;
+// any;
+//strongly datatype In yes we have to tell which type
+// let last_name:string = 'khilji'
+// let age:number = 'khilji'
+// let is_student:boolean = true
+// let Name:any = 'khilji'
+//type inferense It uses itself to determine which type is which
+// let last_name = 'khilji'
+// let age = 'khilji'
+// let is_student = true
+// let Name = 'khilji'
+// Between defferent let and const and using
+// const ki value constant rehti ap os ko change nh kr skte jbk let ki value any time change kr skte h
+// let current_blance = 1000 initial value
+// current_blance = 500  change value
+// const accountid = 'Afsd5466327881743118fgt'
+// let userInput: unknown;
+// userInput = 5;
+// userInput = "Hello";
+// console.log(userInput);
+// let y: null = null;
+// console.log(y); // null
+var y = '%';
---
+++ firstopic.ts
@@ -0,0 +1,63 @@
+// let first_name: string = 'owais'
+// console.log(first_name);
+
+//Datatype types
+// string;
+// number;
+// Boolean;
+// any;
+
+//strongly datatype In yes we have to tell which type
+
+// let last_name:string = 'khilji'
+// let age:number = 'khilji'
+// let is_student:boolean = true
+// let Name:any = 'khilji'
+
+//type inferense It uses itself to determine which type is which
+
+// let last_name = 'khilji'
+// let age = 'khilji'
+// let is_student = true
+// let Name = 'khilji'
+
+// Between defferent let and const and using
+// const ki value constant rehti ap os ko change nh kr skte jbk let ki value any time change kr skte h
+
+// let current_blance = 1000 initial value
+// current_blance = 500  change value
+// const accountid = 'Afsd5466327881743118fgt'
+
+// Advance primative datatype
+
+// udefined
+// let x: undefined;
+// console.log(x); // undefined
+
+// unknown
+
+//null
+// let y: null = null;
+// console.log(y); // null
+
+//symbol
+
+//bgint
+// const bigNumber: bigint = 9007199254740991n;
+// console.log(bigNumber); // 9007199254740991n
+
+//  ERRORS
+
+// syntax error
+// lett Nam = 'owais'
+// console.log(Nam);
+
+//type error
+//let x: number = 10;
+//x = "Hello";
+
+// assignability error
+
+// let message = 'Hello'
+// message = 5
+// console.log(message);
